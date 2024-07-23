57c808bba77882656d426bd3c952354613ec5381
SonHoangPoppingBean <90617745+SonHoangPoppingBean@users.noreply.github.com>
2024-07-23 12:04:58 +0700

    update route to be able to use multiple wallet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
       import "../stone.css";
       import { onMount } from 'svelte';
       
-      import Screen from '$lib/component/hotchest/Screen.svelte';
+      import Screen from '$lib/component/chest/Screen.svelte';
   
       onMount(async () => {
       });
